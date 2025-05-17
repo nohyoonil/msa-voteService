71e669a43b0839a3cbadd66abbea70ac62d7610c
yoon <yunil9631@naver.com>
2025-05-17 14:43:03 +0900

    kafka + redis polling으로 변경

--- src/main/java/org/yoon/msavoteservice/VoteController.java
+++ src/main/java/org/yoon/msavoteservice/VoteController.java
@@ -3,10 +3,7 @@ package org.yoon.msavoteservice;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.yoon.msavoteservice.model.request.VoteInfoReq;
 import org.yoon.msavoteservice.model.response.VoteDetailRes;
 
--- src/main/java/org/yoon/msavoteservice/VoteService.java
+++ src/main/java/org/yoon/msavoteservice/VoteService.java
@@ -3,6 +3,7 @@ package org.yoon.msavoteservice;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.yoon.msavoteservice.kafka.KafkaProducer;
 import org.yoon.msavoteservice.model.dto.VoteDetailDto;
@@ -16,23 +17,44 @@ import java.time.LocalDateTime;
 public class VoteService {
 
     private final KafkaProducer kafkaProducer;
-    private final ObjectMapper objectMapper;
+    private final RedisTemplate<String, String> redisTemplate;
     private final VoteRepository voteRepository;
 
     public VoteDetailRes vote(long userId, VoteInfoReq req) {
+        long questionId = req.getQuestionId();
+        kafkaProducer.send("validate.questionId", String.valueOf(questionId));
+
+        if (!checkReplyInRedis("questionId:" + questionId))
+            throw new RuntimeException("Invalid questionId: " + questionId);
+
+        long targetId = req.getTargetId();
+        kafkaProducer.send("validate.memberId", String.valueOf(targetId));
+
+        if (!checkReplyInRedis("memberId:" + targetId))
+            throw new RuntimeException("Invalid targetId: " + targetId);
+
         Vote vote = voteRepository.save(Vote.builder()
                 .voterId(userId)
-                .targetId(req.getTargetId())
-                .questionId(req.getQuestionId())
+                .targetId(targetId)
+                .questionId(questionId)
                 .createdAt(LocalDateTime.now())
                 .build());
 
-        try {
-            kafkaProducer.send("validate.question", objectMapper.writeValueAsString(VoteDetailDto.from(vote)));
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-
         return VoteDetailRes.from(vote);
     }
+
+    private boolean checkReplyInRedis(String key) {
+        int limit = 30;
+        for (int i = 0; i < limit; i++) {
+            if (redisTemplate.hasKey(key)) return true;
+            try {
+                Thread.sleep(100); // 0.1초
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                return false;
+            }
+        }
+        return false;
+    }
 }
+
