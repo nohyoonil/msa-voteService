c6ccdfab202159158bb18bb9a7fa6b1a668519cb
yoon <yunil9631@naver.com>
2025-05-19 14:00:02 +0900

    url restful 변경

--- src/main/java/org/yoon/msavoteservice/VoteController.java
+++ src/main/java/org/yoon/msavoteservice/VoteController.java
@@ -14,13 +14,13 @@ public class VoteController {
     private final VoteService voteService;
 
     //투표하기
-    @PostMapping("/api/vote")
+    @PostMapping("/api/votes")
     public ResponseEntity<VoteDetailRes> vote(@RequestHeader("X-User-Id") long userId, @RequestBody VoteInfoReq req) {
         return ResponseEntity.status(HttpStatus.CREATED).body(voteService.vote(userId, req));
     }
 
     //투표자 알아내기
-    @PostMapping("/api/vote/open/{voteId}")
+    @PostMapping("/api/votes/open/{voteId}")
     public ResponseEntity<Long> open(@RequestHeader("X-User-Id") long userId, @PathVariable long voteId) {
         return ResponseEntity.status(HttpStatus.CREATED).body(voteService.open(userId, voteId));
     }
